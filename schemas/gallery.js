f4e18abb0433d0c40d22cd36246e23e9e30ab11d
iamkunall <103kunalsharma@gmail.com>
2020-02-25 16:49:24 +0530

    setup admin

---
+++ schemas/gallery.js
@@ -0,0 +1,32 @@
+export default {
+  name: 'gallery',
+  title: 'Gallery',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'image',
+    },
+  },
+};
---
+++ schemas/homePage.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'homepage',
+  title: 'Home Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'subtitle',
+      title: 'Sub Title',
+      type: 'string',
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+    },
+  ],
+};
